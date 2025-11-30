038f4bac374287b1c4d7f9cc730ebcf33b2dd0ea
Ulbrich Frauendorf <ulbrich@invensys.web.za>
2025-11-30 10:00:42 +0200

    Sidebar close in mobile

--- projects/integra-ng/src/lib/components/layout/sidebar/sidebar.component.ts
+++ projects/integra-ng/src/lib/components/layout/sidebar/sidebar.component.ts
@@ -1,15 +1,17 @@
 import { Component, ElementRef, Input } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { MenuModel } from '../models/menu.model';
+import { LayoutService } from '../services/layout.service';
+import { IButton } from '../../button/button.component';
 
 @Component({
   selector: 'i-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  imports: [MenuComponent],
+  imports: [MenuComponent, IButton],
 })
 export class SidebarComponent {
   @Input() menuModel: MenuModel[] = [];
 
-  constructor(public el: ElementRef) {}
+  constructor(public el: ElementRef, public layoutService: LayoutService) {}
 }
